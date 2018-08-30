943808aa05b790a35067619f5f06d70c7ad0d202
zhangke <zk666163@163.com>
2018-08-30 09:13:23 +0800

    编写值班表、短信模板表Models

--- api/app/models.py
+++ api/app/models.py
@@ -1,12 +1,33 @@
 # coding=utf-8
+
+from datetime import datetime
 from api.app import db
 
 
-class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('user', backref='role', lazy='dynamic')
+class BaseModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True, index=True)
+    ctime = db.Column(db.DateTime(timezone=True), nullable=False, default=datetime.now, index=True)
+    lastchange = db.Column(db.DateTime(timezone=True), nullable=True, default=datetime.now, onupdate=datetime.now)
+    status = db.Column(db.Boolean, default=True)
+    remark = db.Column(db.String(128), nullable=True)  # 备用字段
+
+
+class OnDuty(BaseModel, db.Model):
+    __tablename__= 'onduty'
+    department_name = db.Column(db.String(64), nullable=False)
+    principal = db.Column(db.String(64), nullable=False)
+    role_name = db.Column(db.String(64), nullable=False)
+    name = db.Column(db.String(64), nullable=False)
+    duty_time = db.Column(db.Date(), nullable=False, index=True)
 
     def __repr__(self):
-        return '<Role %r>' % self.name
+        return '<OnDuty %r>' % self.name
+
+
+class TempText(BaseModel, db.Model):
+    __tablename__ = 'temptext'
+    name = db.Column(db.String(64))
+    content = db.Column(db.String(256), nullable=False)
+
+    def __repr__(self):
+        return '<TempText %r>' % self.content
