d1c45cf34e24c6b5e865922bfb8d2b97a1ca31e8
houxianghui <565572139@qq.com>
2018-08-31 10:10:59 +0800

    修改User表和添加超级管理员

---
+++ api/app/BlueAuth/datas.py
@@ -0,0 +1,13 @@
+# coding=utf-8
+
+DEFAULT_SUPPER_USER = {
+    'name': 'admin',
+    'alias': u'管理员',
+    'mobile': '10000',
+    'tag': '1',
+    'password': 'a1s2d3f4'
+}
+DEFAULT_SUPPER_ADMIN = {
+    'name': 'ADMIN',
+    'alias': u'超级管理员'
+}
