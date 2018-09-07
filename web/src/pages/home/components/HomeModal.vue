9d61e81d7239ef4558cf9196f92f74005d0e9b85
shenmeifang <15010234215@163.com>
2018-09-07 14:16:07 +0800

    单个或所有部门的某天值班信息

--- web/src/pages/home/components/HomeModal.vue
+++ web/src/pages/home/components/HomeModal.vue
@@ -10,7 +10,82 @@
       <Col span="12" :style="{'height': '100%'}">
         <Card shadow :style="{'height': '100%'}">
           <p slot="title">排班详情</p>
-          <Table border :columns="columns7" :data="data6" :height="440"></Table>
+          <div class="ivu-table-wrapper">
+            <div class="ivu-table ivu-table-default ivu-table-with-fixed-top">
+              <div class="ivu-table-header">
+                <table cellspacing="0" cellpadding="0" border="0" style="width: 680px">
+                  <colgroup>
+                    <col width="200">
+                    <col width="200">
+                    <col width="200">
+                    <col width="74">
+                    <col width="17">
+                  </colgroup>
+                  <thead>
+                  <tr>
+                    <th>
+                      <div class="ivu-table-cell">
+                        <span class="">部门名</span>
+                      </div>
+                    </th>
+                    <th>
+                      <div class="ivu-table-cell">
+                        <span class="">排班角色</span>
+                      </div>
+                    </th>
+                    <th>
+                      <div class="ivu-table-cell">
+                        <span class="">值班人员</span>
+                      </div>
+                    </th>
+                    <th>
+                      <div class="ivu-table-cell">
+                        <span class="">操作</span>
+                      </div>
+                    </th>
+                    <th rowspan="1"></th>
+                  </tr>
+                  </thead>
+                </table>
+              </div>
+              <div class="ivu-table-body ivu-table-overflowY" style="height: 100%">
+                <table cellspacing="0" cellpadding="0" border="0" style="width: 663px">
+                  <colgroup>
+                    <col width="200">
+                    <col width="200">
+                    <col width="200">
+                    <col width="72">
+                  </colgroup>
+                  <tbody class="ivu-table-tbody">
+                  <tr class="ivu-table-row"  v-for="(item, index) in tableList" :key="index">
+                    <td :rowspan="item.col1Rospan" v-if="item.col1RospanShow">
+                      <div class="ivu-table-cell">
+                        <span>{{item.departName}}</span>
+                      </div>
+                    </td>
+                    <td :rowspan="item.col2Rospan" v-if="item.col2RospanShow">
+                      <div class="ivu-table-cell">
+                        <span>{{item.roleName}}</span>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="ivu-table-cell">
+                        <span>{{item.dutyName}}</span>
+                      </div>
+                    </td>
+                    <td>
+                      <div class="ivu-table-cell">
+                      <span>
+                        <Button type="error" size="small" @click="deleteDutyRecord(item.dutyId)">删除</Button>
+                      </span>
+                      </div>
+                    </td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
         </Card>
       </Col>
       <Col span="11" offset="1"  :style="{'height': '100%'}">
@@ -32,12 +107,12 @@
                 <Select v-model="formValidate.staff" placeholder="选择排班人员">
                   <Option value="侯相会">侯相会</Option>
                   <Option value="张可">张可</Option>
-                  <Option value="刘梦溪">刘梦溪</Option>
+                  <Option value="刘梦溪">刘孟希</Option>
                 </Select>
               </FormItem>
               <FormItem>
-                <Button type="primary" @click="handleSubmit('formValidate')">Submit</Button>
-                <Button @click="handleReset('formValidate')" style="margin-left: 8px">Reset</Button>
+                <Button type="primary" @click="handleSubmit('formValidate')">提交</Button>
+                <Button @click="handleReset('formValidate')" style="margin-left: 8px">重置</Button>
               </FormItem>
             </Form>
           </div>
@@ -52,6 +127,7 @@ export default {
   name: 'HomeModal',
   data () {
     return {
+      childTableList: [],
       formValidate: {
         role: '',
         staff: ''
@@ -64,88 +140,13 @@ export default {
           { required: true, message: '排班人员不能为空！', trigger: 'change' }
         ]
       },
-      departOneModalCopy: false,
-      columns7: [
-        {
-          title: '部门',
-          key: 'departName',
-          render: (h, params) => {
-            return h('div', [
-              h('strong', params.row.name)
-            ])
-          }
-        },
-        {
-          title: 'Age',
-          key: 'age'
-        },
-        {
-          title: 'Address',
-          key: 'address'
-        },
-        {
-          title: 'Action',
-          key: 'action',
-          width: 150,
-          align: 'center',
-          render: (h, params) => {
-            return h('div', [
-              h('Button', {
-                props: {
-                  type: 'primary',
-                  size: 'small'
-                },
-                style: {
-                  marginRight: '5px'
-                },
-                on: {
-                  click: () => {
-                    this.show(params.index)
-                  }
-                }
-              }, 'View'),
-              h('Button', {
-                props: {
-                  type: 'error',
-                  size: 'small'
-                },
-                on: {
-                  click: () => {
-                    this.remove(params.index)
-                  }
-                }
-              }, 'Delete')
-            ])
-          }
-        }
-      ],
-      data6: [
-        {
-          name: 'John Brown',
-          age: 18,
-          address: 'New York No. 1 Lake Park'
-        },
-        {
-          name: 'Jim Green',
-          age: 24,
-          address: 'London No. 1 Lake Park'
-        },
-        {
-          name: 'Joe Black',
-          age: 30,
-          address: 'Sydney No. 1 Lake Park'
-        },
-        {
-          name: 'Jon Snow',
-          age: 26,
-          address: 'Ottawa No. 2 Lake Park'
-        }
-      ]
+      departOneModalCopy: false
     }
   },
   props: {
     departOneModal: Boolean,
-    currentDay: String
+    currentDay: String,
+    tableList: Array
   },
   methods: {
     handleSubmit (name) {
@@ -159,17 +160,23 @@ export default {
     },
     handleReset (name) {
       this.$refs[name].resetFields()
+    },
+    deleteDutyRecord (dutyId) {
+      alert(dutyId)
     }
   },
   mounted () {
     this.bus.$on('dayClickWatch', (param) => {
       this.departOneModalCopy = true
-      console.log(this.departOneModal)
     })
+  },
+  updated () {
+    console.log(this.tableList)
   }
 }
 </script>
 
-<style scoped>
-
+<style scoped lang="stylus">
+  *
+    box-sizing border-box
 </style>
