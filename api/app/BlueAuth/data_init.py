102e0c3f6d3dee2feb83524c48c310ed5a3484d4
houxianghui <565572139@qq.com>
2018-09-21 16:12:06 +0800

    添加关系初始化数据，所有权限默认与超级管理组建立关系， 修改权限创建接口，把新权限添加到超级管理组下

--- api/app/BlueAuth/data_init.py
+++ api/app/BlueAuth/data_init.py
@@ -83,12 +83,14 @@ def data_init():
             new_permission.codename = p['codename']
             new_permission.alias = p['alias']
             department_wanted = Department.query.filter_by(name=p.get('department', 'UNKNOW')).first()
+            management_wanted = Management.query.filter_by(name=p.get('management', 'UNKNOW')).first()
             if department_wanted:
+                management_wanted.permissions.append(new_permission)
                 department_wanted.permissions.append(new_permission)
             else:
                 del new_permission
                 continue
-            db.session.add_all([new_permission, department_wanted])
+            db.session.add_all([new_permission, department_wanted, management_wanted])
         try:
             db.session.commit()
         except Exception as e:
